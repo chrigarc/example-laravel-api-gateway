33ee76a364bfd0cfba9ce6f6eba5bb2e8694a4d2
Christian Rafael Garcia Garcia <chrigarc@MacBook-Pro-de-Lapi.local>
2021-02-17 12:44:00 -0600

    endpoints demos

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Models\User;
+use GuzzleHttp\Client;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +21,64 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Broadcast::routes(['middleware' => ['auth:web', 'auth:sanctum']]);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    return new JsonResponse(['data' => $request->user()]);
+});
+
+Route::post('/login', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+    Auth::login($user, true);
+    $user  = Auth::user();
+    $tokenName = strtolower(Str::slug(config('app.name'), '-') . '-token');
+    $token = $user->createToken($tokenName, ['access:broadcast'])->plainTextToken;
+    $user->token = $token;
+    return new JsonResponse(['user' => $user, 'session' => session()->get('a')]);
+});
+
+Route::get('csrf-token', function () {
+    return csrf_token();
+});
+
+
+
+Route::middleware('auth:sanctum')->post('/logout', function (Request $request) {
+    $user = auth()->user();
+    auth('web')->logout();
+    try {
+        session()->flush();
+        $user->tokens()->delete();
+    } catch (\Exception $exception) {
+        info($exception->getMessage(), [
+            'logout' => $user->id
+        ]);
+    }
+    return new \Illuminate\Http\JsonResponse([
+        'message' => 'ok'
+    ]);
+});
+
+
+Route::middleware('auth:sanctum')->get('/test1', function(Request  $request) {
+    $guzzle = new Client();
+    $response = $guzzle->get(env('MICRO_S1'));
+    return json_decode((string)$response->getBody(), true);
+});
+
+Route::middleware('auth:sanctum')->get('/test2', function(Request  $request) {
+    $guzzle = new Client();
+    $response = $guzzle->get(env('MICRO_S2'));
+    return json_decode((string)$response->getBody(), true);
 });
